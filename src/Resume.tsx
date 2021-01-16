732ff41cb5eff14d4e10e68c24eb5fc952ceed09
Tobias Wahyudi <tobi8800@gmail.com>
2021-01-16 00:18:09 -0500

    Resume page sizing and background

---
+++ src/Resume.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Resume.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <div id="page">
+      <div id="header">
+        <div className="left-column">
+          <Title />
+          <p id="summary">
+            Third-year CS at the University of Waterloo. Significantly experienced in React development, and moderately skilled at Full-Stack engineering - including JS, Python, and Java backend libraries. Familiar in working with CI/CD and Agile workspaces.
+          </p>
+        </div>
+        <div className="right-column">
+          x
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
