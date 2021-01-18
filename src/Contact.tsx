5b21ba9968b592da29b8eb3cd4a25c340539b06a
Tobias Wahyudi <tobi8800@gmail.com>
2021-01-18 09:22:56 -0500

    Edit email, remove location from contact

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -4,7 +4,6 @@ import './Contact.css';
 import mailIcon from './res/img/mail.svg';
 import webIcon from './res/img/web.svg';
 import linkedinIcon from './res/img/linkedin.svg';
-import locIcon from './res/img/loc.svg';
 import gitIcon from './res/img/git.svg';
 import phoneIcon from './res/img/phone.svg';
 
@@ -32,17 +31,14 @@ export const ContactRow: React.FC<ContactRowProps> = ({ imgSrc, children, linkTo
 export const Contact: React.FC = () => {
   return (
     <div id="contact">
-      <ContactRow imgSrc={mailIcon} imgAlt="Email address" linkTo="mailto:tobiaswahyudi@gmail.com">
-        tobiaswahyudi@gmail.com
+      <ContactRow imgSrc={mailIcon} imgAlt="Email address" linkTo="mailto:toby.wahyudi@uwaterloo.ca">
+        toby.wahyudi@uwaterloo.ca
       </ContactRow>
       <ContactRow imgSrc={webIcon} imgAlt="Web address" linkTo="https://toby.wahyudi.ca">
         https://toby.wahyudi.ca
       </ContactRow>
       <ContactRow imgSrc={linkedinIcon} imgAlt="LinkedIn profile" linkTo="https://www.linkedin.com/in/tobias-wahyudi/">
         /in/tobias-wahyudi/
-      </ContactRow>
-      <ContactRow imgSrc={locIcon} imgAlt="Based in">
-        Toronto, Ontario
       </ContactRow>
       <ContactRow imgSrc={gitIcon} imgAlt="GitHub address" linkTo="https://github.com/tobiaswahyudi">
         /tobiaswahyudi
