943b08c8be7454c1c0b1a4be7c654d19aac2db90
Tobias Wahyudi <tobi8800@gmail.com>
2021-01-18 01:53:59 -0500

    Remove summary, switch to pure two column layout

--- src/Resume.tsx
+++ src/Resume.tsx
@@ -10,27 +10,16 @@ import { Work } from './Work';
 export const Resume: React.FC = () => {
   return (
     <div id="page">
-      <div id="header">
         <div className="left-column">
           <Title />
-          <p id="summary">
-            Significantly experienced in React development, and moderately skilled at Full-Stack engineering - including JS, Python, and Java backend libraries. Well-rounded with experience in leadership and design.
-          </p>
-        </div>
-        <div className="right-column">
-          <Contact />
-        </div>
-      </div>
-      <div id="content">
-        <div className="left-column">
           <Work />
         </div>
         <div className="right-column">
+          <Contact />
           <Education />
           <Skills />
           <Activities />
         </div>
-      </div>
     </div>
   );
 }
